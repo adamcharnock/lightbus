7c4513719919f7ae6081249281344bace22bc4e3
Adam Charnock <adam@adamcharnock.com>
2017-10-18 07:17:00 +0100

    Removing old Event implementation

--- lightbus/api.py
+++ lightbus/api.py
@@ -84,39 +84,3 @@ class Event(object):
     def __init__(self, arguments):
         # Ensure you update the __copy__() method if adding instance variables below
         self.arguments = arguments
-        # self._api = None
-
-    # def __get__(self, api_instance, type=None):
-    #     self._bind_api(api_instance)
-    #     return self
-    #
-    # def __copy__(self):
-    #     """Produce an unbound copy of this event"""
-    #     return Event(arguments=self.arguments)
-    #
-    # def dispatch(self, **kwargs):
-    #     # Correction, perhaps the event should not do the dispatching.
-    #     # Perhaps the client should proxy through calls to events and
-    #     # just read metadata from the event.
-    #     pass
-    #     import pdb; pdb.set_trace()
-    #
-    # def listen(self):
-    #     pass
-    #
-    # def _bind_api(self, api_instance):
-    #     if not isinstance(api_instance, Api):
-    #         raise InvalidApiEventConfiguration(
-    #             "It appears you are trying to use an event attached to "
-    #             "non-API object {}. Events should be defined as class "
-    #             "attributes upon your Api class(es). See the API definition "
-    #             "documentation for further details.".format(api_instance)
-    #         )
-    #     if self._api is not None and self._api is not api_instance:
-    #         raise InvalidApiEventConfiguration(
-    #             "Attempting to use the same Event instance upon two different "
-    #             "Apis is unsupported. If you really wish to do this you "
-    #             "should copy the event."
-    #         )
-    #     if self._api is None:
-    #         self._api = api_instance
