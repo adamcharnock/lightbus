dbbbabb7c62cd0dbd1c2383c084b6ac9ba87656c
Adam Charnock <adam@adamcharnock.com>
2017-11-26 20:59:55 +0000

    Adding aioredis requirement

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'lightbus = lightbus.commands:lightbus_entry_point'
+            'lightbus = lightbus.commands:lightbus_entry_point',
+            'aioredis',
         ]
     }
 )
