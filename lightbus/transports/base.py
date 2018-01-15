382246e4fadb7f1c5c977c4a8109b1f6a590a7d5
Adam Charnock <adam@adamcharnock.com>
2018-01-15 01:55:59 +0000

    Initial thoughts on EventTransport.get_listener_group_key()

--- lightbus/transports/base.py
+++ lightbus/transports/base.py
@@ -1,4 +1,5 @@
-from typing import Sequence, Tuple, Any
+from typing import Sequence, Tuple, Any, Hashable
+from uuid import uuid1
 
 from lightbus.api import Api
 from lightbus.message import RpcMessage, EventMessage, ResultMessage
@@ -106,3 +107,48 @@ class EventTransport(object):
     async def stop_listening_for(self, api_name: str, event_name: str):
         """Instruct this transport to stop listening for the given event"""
         pass
+
+    def get_listener_group_key(self, api_name: str, event_name: str, options: dict) -> Hashable:
+        """Get a key to uniquely identify a group of listeners for the given parameters
+
+        This method allows the backend to control how the bus connects incoming
+        events to listeners.
+
+        There are two scenarios possible when the bus' listen() method is called:
+
+            1. Call the EventTransport's start_listening_for() method and subsequently expect
+               the event transport to provide the relevant events (via fetch_events()).
+               Upon receiving these events the bus will call the callback provided to listen().
+
+            2. Realise the specified event is already being consumed by the EventTransport in question.
+               Therefore no call to start_listening_for() is needed, and the provided callable
+               can just be add to the list of things to call upon receiving the specified events.
+
+        Scenario 2 in effect creates a grouping of listeners. The EventTransport is consuming a single
+        stream of events and passing each event to the bus, but the bus is calling multiple listeners callbacks for
+        each event. This reduces the number of network connections, reduces network traffic, and reduces the
+        number of messages which need to be decoded.
+
+        However, in some cases grouping listeners together does not make sense. For example, consider a
+        system where past events are available. One listener may just want to listen for any
+        auth.user_signup events in real-time. On the other hand, another listener may want
+        to do the same but to start by streaming all events ever.
+
+        In the former case the callback only wants events as they happen (maybe it is responsible
+        for sending the signup emails to new users), whereas the second case expects all signup events ever
+        followed by a real-time stream (maybe it is populating a database or report).
+
+        The RedisEventTransport supports this, and be be specified via ``options['since']``.
+
+        We therefore need a way to distinguish between these two cases. Otherwise it would be easy
+        to accidentally re-send all your signup emails to every user ever, for example.
+
+        You should implement this method to return different values for listeners which cannot
+        be grouped together, and to return the same values for listeners which can be grouped together.
+
+        The default implementation simply returns a random UUID in all cases, therefore
+        no grouping will occur. This is the safest option, but also will not provide any
+        of the performance benefits details above.
+
+        """
+        return uuid1()
