53d42e06e03bb690643deeca4c0a78edbc268a72
Adam Charnock <adam@adamcharnock.com>
2018-06-04 16:46:47 +0100

    Lightbus JSON encoder can now handle UUIDs, and adding json_safe_values() utility method

--- lightbus/schema/encoder.py
+++ lightbus/schema/encoder.py
@@ -2,6 +2,7 @@ from json import JSONEncoder
 
 from datetime import datetime, date
 from enum import Enum
+from uuid import UUID
 
 from lightbus.utilities.frozendict import frozendict
 
@@ -16,9 +17,26 @@ class LightbusEncoder(JSONEncoder):
             return o.value
         elif isinstance(o, (datetime, date)):
             return o.isoformat()
+        elif isinstance(o, UUID):
+            return str(o)
         else:
             super(LightbusEncoder, self).default(o)
 
 
 def json_encode(obj, indent=2, sort_keys=True, **options):
     return LightbusEncoder(indent=indent, sort_keys=sort_keys, **options).encode(obj)
+
+
+def json_safe_values(d: dict) -> dict:
+    for k, v in d.items():
+        if isinstance(v, dict):
+            d[k] = json_safe_values(v)
+        elif isinstance(v, frozendict):
+            d[k] = v._dict
+        elif isinstance(v, Enum):
+            d[k] = v.value
+        elif isinstance(v, (datetime, date)):
+            d[k] = v.isoformat()
+        elif isinstance(v, UUID):
+            d[k] = str(v)
+    return d
