5cd7e63bfedb690a6bf5b442b25e84d3641fa28e
Adam Charnock <adam@adamcharnock.com>
2018-06-06 16:04:51 +0100

    Initial work on mocker for testing use of bus

--- lightbus/utilities/testing.py
+++ lightbus/utilities/testing.py
@@ -1,11 +1,19 @@
 import logging
 import os
-from contextlib import contextmanager
+from contextlib import contextmanager, ContextDecorator
+from copy import copy
+from functools import wraps
+from unittest.mock import patch
 
 from typing import List, Dict
 
+from lightbus import BusNode, RpcTransport, EventTransport, SchemaTransport, ResultTransport
+from lightbus.transports.base import TransportRegistry
+
 _registry: Dict[str, List] = {}
 logger = logging.getLogger(__name__)
+
+# Work in progress for testing crashes
 
 
 def test_hook(hook_name: str):
@@ -25,3 +33,128 @@ def register_callback(hook_name, callback):
 @contextmanager
 def crash_at(hook_name):
     return register_callback(hook_name, os._exit)
+
+
+# Utility for mocking bus calls
+
+
+class MockResult(object):
+
+    def __init__(self, rpc_transport, result_transport, event_transport, schema_transport):
+        self.rpc: TestRpcTransport = rpc_transport
+        self.result: TestResultTransport = result_transport
+        self.event: TestEventTransport = event_transport
+        self.schema: TestSchemaTransport = schema_transport
+
+    def assertEventFired(self, api_name, event_name, times=None):
+        fired_events = [(em.api_name, em.event_name) for em, options in self.event.events]
+        wanted = (api_name, event_name)
+        assert wanted in fired_events, f"Event {api_name}.{event_name} was never fired"
+
+        if times:
+            total_times_fired = len([v for v in fired_events if v == wanted])
+            assert (
+                total_times_fired == times
+            ), f"Event fired the incorrect number of times. " f"Expected {times}, actual {total_times_fired}"
+
+    def __repr__(self):
+        return f"<MockResult: events: {len(self.event.events)}>"
+
+
+class BusMocker(ContextDecorator):
+
+    def __init__(self, bus: BusNode):
+        self.bus = bus
+
+    # Overriding ContextDecorator to pass the mock result to the function
+    def __call__(self, func):
+
+        @wraps(func)
+        def inner(*args, **kwds):
+            with self._recreate_cm() as mock_result:
+                return func(*args, mock_result, **kwds)
+
+        return inner
+
+    def __enter__(self):
+        rpc = TestRpcTransport()
+        result = TestResultTransport()
+        event = TestEventTransport()
+        schema = TestSchemaTransport()
+
+        new_registry = TransportRegistry()
+        new_registry.set_schema_transport(schema)
+
+        self.old_transport_registry = self.bus.bus_client.transport_registry
+
+        for api_name, entry in self.old_transport_registry._registry.items():
+            new_registry.set_rpc_transport(api_name, rpc)
+            new_registry.set_result_transport(api_name, result)
+
+            if hasattr(entry.event, "child_transport"):
+                parent_transport = copy(entry.event)
+                parent_transport.child_transport = event
+                new_registry.set_event_transport(api_name, parent_transport)
+            else:
+                new_registry.set_event_transport(api_name, event)
+
+        self.bus.bus_client.transport_registry = new_registry
+        return MockResult(rpc, result, event, schema)
+
+    def __exit__(self, exc_type, exc, exc_tb):
+        self.bus.bus_client.transport_registry = self.old_transport_registry
+
+
+bus_mocker = BusMocker
+
+
+class TestRpcTransport(RpcTransport):
+
+    def __init__(self):
+        self.rpcs = []
+
+    async def call_rpc(self, rpc_message, options: dict):
+        self.rpcs.append((rpc_message, options))
+
+    async def consume_rpcs(self, apis):
+        raise NotImplementedError("Not yet supported by mocks")
+
+
+class TestResultTransport(ResultTransport):
+
+    def get_return_path(self, rpc_message):
+        return "test://"
+
+    async def send_result(self, rpc_message, result_message, return_path):
+        raise NotImplementedError("Not yet supported by mocks")
+
+    async def receive_result(self, rpc_message, return_path, options):
+        raise NotImplementedError("Not yet supported by mocks")
+
+
+class TestEventTransport(EventTransport):
+
+    def __init__(self):
+        self.events = []
+
+    async def send_event(self, event_message, options):
+        self.events.append((event_message, options))
+
+    async def fetch(self, listen_for, context, loop, consumer_group, **kwargs):
+        """Consume RPC events for the given API"""
+        raise NotImplementedError("Not yet supported by mocks")
+
+
+class TestSchemaTransport(SchemaTransport):
+
+    def __init__(self):
+        self.schemas = {}
+
+    async def store(self, api_name: str, schema: Dict, ttl_seconds: int):
+        self.schemas[api_name] = schema
+
+    async def ping(self, api_name: str, schema: Dict, ttl_seconds: int):
+        pass
+
+    async def load(self) -> Dict[str, Dict]:
+        return self.schemas
