6fab46dcf13cd2b2f97eae4e9412623dd9ec350a
Adam Charnock <adam@adamcharnock.com>
2018-06-28 11:21:39 +0100

    making BusPath available on the root lightbus module

--- lightbus/__init__.py
+++ lightbus/__init__.py
@@ -1,6 +1,7 @@
 from .utilities.logging import configure_logging
 from .transports import *
 from .client import *
+from .path import *
 from .message import *
 from .api import *
 from .message import *
