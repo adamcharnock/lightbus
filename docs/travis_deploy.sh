e352718fc9c4d9efdf27d83e3a770ed85c223da4
Adam Charnock <adam@adamcharnock.com>
2018-09-16 16:07:18 +0100

    Work on building docs in travis

--- docs/travis_deploy.sh
+++ docs/travis_deploy.sh
@@ -7,4 +7,7 @@ openssl aes-256-cbc -K $encrypted_739cc9c14904_key -iv $encrypted_739cc9c14904_i
 chmod 600 docs/deploy_key
 ssh-add docs/deploy_key
 
+git remote remove origin
+git remote add origin git@github.com:adamcharnock/lightbus.git
+
 mkdocs gh-deploy
