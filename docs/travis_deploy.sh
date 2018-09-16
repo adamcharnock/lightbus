0a9a09c6548a7c6c04cd43227797b9a1fdc463ec
Adam Charnock <adam@adamcharnock.com>
2018-09-16 16:33:07 +0100

    Work on building docs in travis

--- docs/travis_deploy.sh
+++ docs/travis_deploy.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-openssl aes-256-cbc -K $encrypted_739cc9c14904_key -iv $encrypted_739cc9c14904_iv -in docs/deploy_key.enc -out docs/deploy_key -d
+openssl aes-256-cbc -K $encrypted_739cc9c14904_key -iv $encrypted_739cc9c14904_iv -in docs/deploy_key.enc -out deploy_key -d
 
 set -x
 
 eval "$(ssh-agent -s)"
-chmod 600 docs/deploy_key
-ssh-add docs/deploy_key
+chmod 600 deploy_key
+ssh-add deploy_key
+rm deploy_key
 
 git remote remove origin
 git remote add origin git@github.com:adamcharnock/lightbus.git
 
 mkdocs gh-deploy --force
+
