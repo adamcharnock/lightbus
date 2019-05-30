2f7225d18eabf7d19619338bd6f34528a3c42fd6
Adam Charnock <adam@adamcharnock.com>
2019-05-30 01:24:09 +0100

    Fixes to redis reliabilioty tests and changing how redis bus fixture is created

--- tests/redis_transports/conftest.py
+++ tests/redis_transports/conftest.py
@@ -58,35 +58,13 @@ async def redis_schema_transport(new_redis_pool, loop):
 
 
 @pytest.yield_fixture
-def bus(loop, redis_server_url):
-    # fmt: off
-    bus = lightbus.creation.create(
-        config=RootConfig(
-            apis={
-                'default': ApiConfig(
-                    rpc_transport=RpcTransportSelector(redis=RedisRpcTransport.Config(url=redis_server_url)),
-                    result_transport=ResultTransportSelector(redis=RedisResultTransport.Config(url=redis_server_url)),
-                    event_transport=EventTransportSelector(redis=RedisEventTransport.Config(
-                        url=redis_server_url,
-                        stream_use=StreamUse.PER_EVENT,
-                        service_name="test_service",
-                        consumer_name="test_consumer",
-                    )),
-                )
-            },
-            bus=BusConfig(
-                schema=SchemaConfig(
-                    transport=SchemaTransportSelector(redis=RedisSchemaTransport.Config(url=redis_server_url)),
-                )
-            )
-        ),
-        plugins=[],
-    )
-    # fmt: on
+async def bus(loop, redis_server_url):
+    bus = await new_bus(loop, redis_server_url)()
+
     yield bus
 
     try:
-        bus.client.close()
+        await bus.client.close_async()
     except BusAlreadyClosed:
         pass
 
@@ -102,29 +80,45 @@ def fire_dummy_events_fixture(bus):
     return fire_dummy_events
 
 
+# fmt: off
 @pytest.fixture
-def new_bus(loop, redis_server_config):
-    async def wrapped():
-        rpc_pool = await create_redis_pool(**redis_server_config, loop=loop, maxsize=1000)
-        result_pool = await create_redis_pool(**redis_server_config, loop=loop, maxsize=1000)
-        event_pool = await create_redis_pool(**redis_server_config, loop=loop, maxsize=1000)
-        schema_pool = await create_redis_pool(**redis_server_config, loop=loop, maxsize=1000)
-
-        return await lightbus.creation.create_async(
-            rpc_transport=lightbus.RedisRpcTransport(redis_pool=rpc_pool),
-            result_transport=lightbus.RedisResultTransport(redis_pool=result_pool),
-            event_transport=lightbus.RedisEventTransport(
-                redis_pool=event_pool, service_name="test_service", consumer_name="test_consumer"
+def new_bus(loop, redis_server_url):
+    async def _new_bus():
+        bus = await lightbus.creation.create_async(
+            config=RootConfig(
+                apis={
+                    'default': ApiConfig(
+                        rpc_transport=RpcTransportSelector(
+                            redis=RedisRpcTransport.Config(url=redis_server_url)
+                        ),
+                        result_transport=ResultTransportSelector(
+                            redis=RedisResultTransport.Config(url=redis_server_url)
+                        ),
+                        event_transport=EventTransportSelector(redis=RedisEventTransport.Config(
+                            url=redis_server_url,
+                            stream_use=StreamUse.PER_EVENT,
+                            service_name="test_service",
+                            consumer_name="test_consumer",
+                        )),
+                    )
+                },
+                bus=BusConfig(
+                    schema=SchemaConfig(
+                        transport=SchemaTransportSelector(redis=RedisSchemaTransport.Config(url=redis_server_url)),
+                    )
+                )
             ),
-            schema_transport=lightbus.RedisSchemaTransport(redis_pool=schema_pool),
+            plugins=[],
         )
-
-    return wrapped
+        return bus
+    return _new_bus
+# fmt: on
 
 
 @pytest.fixture
 def get_total_redis_connections(redis_client):
     async def _get_total_redis_connections():
-        return int((await redis_client.info())["clients"]["connected_clients"])
+        info = await redis_client.info()
+        return int(info["clients"]["connected_clients"])
 
     return _get_total_redis_connections
--- tests/redis_transports/test_reliability_redis_rpc.py
+++ tests/redis_transports/test_reliability_redis_rpc.py
@@ -14,42 +14,66 @@ pytestmark = pytest.mark.reliability
 
 
 @pytest.mark.asyncio
-async def test_timeouts(bus: lightbus.path.BusPath, caplog, dummy_api, loop):
-    await bus.client.register_api_async(dummy_api)
+async def test_many_calls_and_clients(bus: lightbus.path.BusPath, new_bus, caplog, dummy_api, loop):
     caplog.set_level(logging.WARNING)
     loop.slow_callback_duration = 0.01
     results = []
 
-    async def do_single_call(n):
+    async def do_single_call(client_bus):
+        nonlocal results
+        result = await client_bus.my.dummy.my_proc.call_async(field="x")
+        results.append(result)
+
+    client_buses = [await new_bus() for n in range(0, 100)]
+    server_buses = [await new_bus() for n in range(0, 10)]
+
+    for server_bus in server_buses:
+        server_bus.client.register_api_async(dummy_api)
+        await server_bus.client.consume_rpcs(apis=[dummy_api])
+
+    # Perform a lot of calls in parallel
+    await asyncio.gather(*[do_single_call(client_bus) for client_bus in client_buses])
+
+    for bus_ in server_buses + client_buses:
+        await bus_.client.close_async()
+
+    assert len(results) == 100
+
+
+@pytest.mark.asyncio
+async def test_timeouts(bus: lightbus.path.BusPath, new_bus, caplog, dummy_api, loop):
+    caplog.set_level(logging.WARNING)
+    loop.slow_callback_duration = 0.01
+    results = []
+
+    async def do_single_call(n, client_bus):
         nonlocal results
         try:
-            result = await bus.my.dummy.random_death.call_async(n=n, death_probability=0.5)
+            result = await client_bus.my.dummy.random_death.call_async(
+                n=n, death_every=10, bus_options={"timeout": 0.1}
+            )
             results.append(result)
         except LightbusTimeout:
             results.append(None)
 
-    async def co_call_rpc():
-        await asyncio.sleep(0.1)
-        fut = asyncio.gather(*[do_single_call(n) for n in range(0, 100)])
-        await fut
-        return fut.result()
+    client_buses = [await new_bus() for n in range(0, 100)]
+    # Create a lot of servers so we have enough to handle all the RPCs before the timeout
+    server_buses = [await new_bus() for n in range(0, 100)]
 
-    async def co_consume_rpcs():
-        return await bus.client.consume_rpcs(apis=[dummy_api])
+    for server_bus in server_buses:
+        server_bus.client.register_api_async(dummy_api)
+        await server_bus.client.consume_rpcs(apis=[dummy_api])
 
-    (call_task,), (consume_task,) = await asyncio.wait(
-        [co_call_rpc(), co_consume_rpcs()], return_when=asyncio.FIRST_COMPLETED
+    # Perform a lot of calls in parallel
+    await asyncio.gather(
+        *[do_single_call(n, client_bus) for n, client_bus in enumerate(client_buses)]
     )
-    call_task.result()
-    consume_task.cancel()
-    try:
-        await consume_task
-        consume_task.result()
-    except CancelledError:
-        pass
+
+    for bus_ in server_buses + client_buses:
+        await bus_.client.close_async()
 
     total_successful = len([r for r in results if r is not None])
     total_timeouts = len([r for r in results if r is None])
     assert len(results) == 100
-    assert total_successful > 0
-    assert total_timeouts > 0
+    assert total_timeouts == 10
+    assert total_successful == 90
