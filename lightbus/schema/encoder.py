0ee79a10aa5e5f1349a08089e70109f7bad0a5ba
Adam Charnock <adam@adamcharnock.com>
2021-01-22 11:17:07 +0000

    No longer encoding json with whitespace

--- lightbus/schema/encoder.py
+++ lightbus/schema/encoder.py
@@ -1,7 +1,7 @@
 from json import JSONEncoder
 
 
-def json_encode(obj, indent=2, sort_keys=True, **options):
+def json_encode(obj, indent=None, sort_keys=True, **options):
     # TODO: This is also used for non-schema related encoding. Either move
     #       this elsewhere, or create a new general purpose encoder
     return JSONEncoder(indent=indent, sort_keys=sort_keys, **options).encode(obj)
